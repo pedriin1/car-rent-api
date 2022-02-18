11aadde7566d2cf0d0dbba5b007210c2236367d0
pedriin1 <frigopedro@icloud.com>
2022-02-18 15:35:27 -0300

    Categories repository created

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,21 +1,14 @@
 import { Router } from "express";
-import { v4 as uuidV4 } from "uuid";
-import { Category } from "../model/Category";
-const categoriesRoutes = Router();
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
-const categories: Category[] = [];
+const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (req, res) => {
   const { name, description } = req.body;
 
-  const category = new Category();
+  categoriesRepository.create({ name, description });
 
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date(),
-  });
-  categories.push(category);
   return res.status(201).send();
 });
 export { categoriesRoutes };
